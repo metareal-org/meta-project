970eb6d44e368814c26ee2bacbfafa00a2aab897
danialzadrafiee <leylasarafrezai@gmail.com>
2024-07-11 11:20:52 +0330

    update

--- next-meta/next.config.mjs
+++ next-meta/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    images: {
-        domains: ['models.readyplayer.me'],
-    },
-
 }
 export default nextConfig;
